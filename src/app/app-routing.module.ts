be36bb39bb4793420bb74276aea2026371adab07
Prem6369 <peterpremkvkv@gmail.com>
2024-02-10 12:48:22 +0530

    done

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'',component:NavbarComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
