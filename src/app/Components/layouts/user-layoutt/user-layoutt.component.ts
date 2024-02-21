8fec8353b253dc98af00ef2a880b6cba2daa947f
its-elango <elangomano17@gmail.com>
2024-02-21 20:40:09 +0530

    added user ,admin layouts and worked UI for add employee page,minor html code change in resort details and rooms and added jquery

---
+++ src/app/Components/layouts/user-layoutt/user-layoutt.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-layoutt',
+  templateUrl: './user-layoutt.component.html',
+  styleUrl: './user-layoutt.component.scss'
+})
+export class UserLayouttComponent {
+
+}
