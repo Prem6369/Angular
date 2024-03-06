54216641eeb64a8cc9a5f3a9160fde7013723060
Prem6369 <peterpremkvkv@gmail.com>
2024-03-06 20:23:34 +0530

    chnage signiup buttn and aadd validator ad change api calling

--- src/app/Components/resort-signup/resort-signup.component.ts
+++ src/app/Components/resort-signup/resort-signup.component.ts
@@ -1,8 +1,9 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ApiServiceRepo } from '../../Repository/resort_repository';
 import { apiLoginService } from '../../Repository/login_repository';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-resort-signup',
@@ -12,44 +13,64 @@ import { apiLoginService } from '../../Repository/login_repository';
 export class ResortSignupComponent implements OnInit {
 
   newuser = new FormGroup({
-    first_name: new FormControl(),
-    last_name: new FormControl(),
-    claySys_email: new FormControl(),
-    phone_number: new FormControl(),
-    address: new FormControl(),
-    role: new FormControl(),
-    lead: new FormControl(),
-    employee_id: new FormControl(),
-    age: new FormControl(),
-    gender: new FormControl(),
-    official_id_card_no: new FormControl(),
-    official_id_image_url: new FormControl(),
-    claysys_id_card_image_url: new FormControl(),
-    allotted_stays:new FormControl(0),
-    used_stays:new FormControl(0),
-    status:new FormControl('active'),
-    username: new FormControl()
+    first_name: new FormControl(null,Validators.required),
+    last_name: new FormControl(null,Validators.required),
+    claySys_email: new FormControl(null,Validators.required),
+    phone_number: new FormControl(null,Validators.required),
+    address: new FormControl(null,Validators.required),
+    role: new FormControl(null,Validators.required),
+    lead: new FormControl(null,Validators.required),
+    employee_id: new FormControl(null,Validators.required),
+    age: new FormControl(null,Validators.required),
+    gender: new FormControl(null,Validators.required),
+    official_id_card_no: new FormControl(null,Validators.required),
+    official_id_image_url: new FormControl(null,Validators.required),
+    claysys_id_card_image_url: new FormControl(null,Validators.required),
+    allotted_stays:new FormControl(),
+    used_stays:new FormControl(),
+    status:new FormControl(''),
+    username: new FormControl(null,Validators.required)
   });
 
-  constructor(private http: HttpClient,private repository:apiLoginService) {}
+  constructor(private http: HttpClient,private repository:apiLoginService,private router:Router) {}
 
   ngOnInit() {}
 
   register() {
+    debugger;
+    if(this.newuser.valid){
+    this.setValues();
     const formData = this.newuser.value; 
     console.log(formData); 
   
-    this.repository.signin(formData).subscribe(
+    this.repository.signup(formData).subscribe(
       (response) => {
         console.log(response);
+        this.router.navigate([''])
+        this.newuser.reset(); 
+
       },
       (error) => {
         console.error('Error occurred during registration:', error);
       }
     );
+  }
+  else{
+    alert('Please fill the all values')
+  }
+
   
-    this.newuser.reset(); 
   }
   
 
+  setValues(){
+    this.newuser.controls['allotted_stays'].setValue(0);
+    this.newuser.controls['status'].setValue('active');
+    this.newuser.controls['used_stays'].setValue(0);
+
+  }
+
+  back(){
+    this.router.navigate([''])
+  }
 }
